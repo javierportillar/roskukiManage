fc2ad870acd14f3dcdda3131499bc80999736f4c
“javierportillar” <javierandres.008@hotmail.com>
2025-06-08 18:02:58 -0500

    base updated for githubpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  // 👇 base en la RAÍZ del objeto de configuración
+  base: '/roskukiManage/',
 });
