3c5d72eebc9fd3ccbc7afc8393052e595a087456
“javierportillar” <javierandres.008@hotmail.com>
2025-06-19 10:10:04 -0500

    set page settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // 👇 base en la RAÍZ del objeto de configuración
-  base: '/',
-  // base: '/roskukiManage/',
+  // base: '/',
+  base: '/roskukiManage/',
 });
